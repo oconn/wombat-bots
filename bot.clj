c2ede4d97954a4f66f95a7acfbbdf1b8ae93a0b4
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-06-22 11:42:24 -0400

    update

--- bot.clj
+++ bot.clj
@@ -1,15 +1,5 @@
-(defn run2 [] (println "run 2"))
-
 (defn run
   [arena saved-state my-id]
-  (println "run 1")
-  {:type "MOVE"
-   :metadata {:direction (rand-nth [0 1 2 3 4])}
-   :saved-state {}})
-
-(defn run3
-  [arena saved-state my-id]
-  (println "run 3")
   {:type "MOVE"
    :metadata {:direction (rand-nth [0 1 2 3 4])}
    :saved-state {}})
