d91b652abafa33038d84c8f289e180dbb7dcbc1b
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-06-24 20:33:18 -0400

    update bot protocol

--- bot.clj
+++ bot.clj
@@ -1,5 +1,6 @@
 (defn run
   [arena saved-state my-id]
-  {:type "MOVE"
-   :metadata {:direction (rand-nth [0 1 2 3 4])}
-   :saved-state {}})
+  {:commands [{:cmd "MOVE"
+               :metadata {:direction (rand-nth [0 1 2 3 4])}}
+              {:cmd "SET_STATE"
+               :metadata {:foo "bar"}}]})
