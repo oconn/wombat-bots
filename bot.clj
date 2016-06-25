4615f562e1ff32865f5cccdddb87af0cc1b8f872
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-06-24 20:53:41 -0400

    update bot example

--- bot.clj
+++ bot.clj
@@ -1,5 +1,5 @@
 (defn run
-  [arena saved-state my-id]
+  [{:keys [arena state bot_id energy spawn-bot?] :as step-details}]
   {:commands [{:cmd "MOVE"
                :metadata {:direction (rand-nth [0 1 2 3 4])}}
               {:cmd "SET_STATE"
