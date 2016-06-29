ae576c96646f046911bd2859dcc4f1cce22bff35
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-06-28 21:26:59 -0400

    update bot

--- bot.clj
+++ bot.clj
@@ -1,5 +1,19 @@
+(defn pprint-arena
+  "Pretty Print for a given arena"
+  [arena]
+  (let [[x-len _] (get-arena-dimensions arena)
+        x-indices (range x-len)]
+    (println " " (string/join " " (vec x-indices)))
+    (print
+     (string/join "\n" (map-indexed (fn [idx row]
+                                      (print
+                                       idx
+                                       (string/join " " (map :display row))
+                                       "\n")) arena)))))
+
 (defn run
   [{:keys [arena state bot_id energy spawn-bot?] :as step-details}]
+  (pprint-arena arena)
   {:commands [{:cmd "MOVE"
                :metadata {:direction (rand-nth [0 1 2 3 4])}}
               {:cmd "SET_STATE"
