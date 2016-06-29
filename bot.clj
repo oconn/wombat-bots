d2468b47b5e44ce0de8ec413dca24efb41eeb3f2
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-06-29 19:11:19 -0400

    update bot

--- bot.clj
+++ bot.clj
@@ -22,7 +22,7 @@
 
 (defn run
   [{:keys [arena saved-state bot-id energy spawn-bot?] :as step-details}]
-  (let [counter (or (:counter saved-state) 0)]
+  (let [counter (or (:step-counter saved-state) 0)]
     (println "Bot Counter: " counter)
     {:commands [{:cmd "MOVE"
                  :metadata {:direction (rand-nth [0 1 2 3 4])}}
