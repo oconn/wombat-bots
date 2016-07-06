184cad3b0eb635071df09a418128e3f2587511dd
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-07-05 20:15:59 -0400

    update bot

--- bot.clj
+++ bot.clj
@@ -21,9 +21,9 @@
                                        "\n")) arena)))))
 
 (defn run
-  [{:keys [arena saved-state bot-id energy spawn-bot?] :as step-details}]
-  (let [counter (or (:step-counter saved-state) 0)]
-    {:commands [{:cmd "MOVE"
-                 :metadata {:direction (rand-nth [0])}}
-                {:cmd "SET_STATE"
-                 :metadata {:step-counter (inc counter)}}]}))
+  [{:keys  [arena state bot_id energy spawn-bot?] :as step-details}]
+  (println  "arena -- "  (count arena))
+  {:commands  [{:cmd  "SHOOT"
+                :metadata  {:direction  (rand-nth  [0 1 2 3 4]) :energy 1}}
+               {:cmd  "SET_STATE"
+                :metadata  {:foo  "bar"}}]})
