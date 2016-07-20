0086775b6f4776660e9743b2cc827d28975060d7
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-07-20 11:19:09 -0400

    update bot

--- bot.clj
+++ bot.clj
@@ -22,10 +22,10 @@
 
 (defn run
   [{:keys  [arena state bot_id energy spawn-bot?] :as step-details}]
-  ;; {:cmd  "SHOOT"
-  ;;  :metadata  {:direction (rand-nth  [0 1 2 3 4]) :energy 100}}
-  (let [command-options [[{:cmd  "SET_STATE"
+  (let [command-options [[{:cmd  "SHOOT"
+                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7]) :energy 20}}
+                          {:cmd  "SET_STATE"
                            :metadata  {:foo  "bar"}}]
                          [{:cmd  "MOVE"
-                           :metadata  {:direction (rand-nth  [0 1 2 3 4])}}]]]
+                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]]]
     {:commands (rand-nth command-options)}))
