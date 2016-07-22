c78daf99fcedb050147e5c199e8f61c19f58525c
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-07-22 08:23:45 -0400

    Update wombat!

--- bot.clj
+++ bot.clj
@@ -1,25 +1,3 @@
-(require '[clojure.string :as string])
-
-(defn get-arena-dimensions
-  "returns the dimensions of a given arena (NOTE: Not 0 based)"
-  [arena]
-  (let [x ((comp count first) arena)
-        y (count arena)]
-    [x y]))
-
-(defn pprint-arena
-  "Pretty Print for a given arena"
-  [arena]
-  (let [[x-len _] (get-arena-dimensions arena)
-        x-indices (range x-len)]
-    (println " " (string/join " " (map #(format "%2d" %) x-indices)))
-    (print
-     (string/join "\n" (map-indexed (fn [idx row]
-                                      (print
-                                       (format "%2d" idx)
-                                       (string/join "  " (map :display row))
-                                       "\n")) arena)))))
-
 (defn run
   [{:keys  [arena state bot_id energy spawn-bot?] :as step-details}]
   (let [command-options [[{:cmd  "SHOOT"
