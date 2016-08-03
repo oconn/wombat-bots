8d3d7efe5c9ae3c38f43506c5d61640923e27f57
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-08-03 08:40:12 -0400

    Update wombat!

--- bot.clj
+++ bot.clj
@@ -1,7 +1,8 @@
 (defn run
   [{:keys  [arena state bot_id energy spawn-bot?] :as step-details}]
-  (let [command-options [[{:cmd  "SHOOT"
-                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7]) :energy 20}}]
+  (let [command-options [
+                         ;; [{:cmd  "SHOOT"
+                         ;;   :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7]) :energy 20}}]
                          [{:cmd  "MOVE"
                            :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]]]
     {:commands (rand-nth command-options)}))
