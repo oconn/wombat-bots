96061e5fffaaed5515736614e28f1f4ff87c6e85
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-02-08 10:33:22 -0500

    add somke command

--- bot-one.clj
+++ bot-one.clj
@@ -1,11 +1,18 @@
 (fn [state time-left]
   (def turn-directions [:right :left :about-face])
 
-  (let [command-options [(repeat 10 {:command {:action :move
-                                               :metadata {}}})
-                         (repeat 2 {:command {:action :turn
-                                              :metadata {:direction (rand-nth turn-directions)}}})
-                         (repeat 4 {:command {:action :shoot
-                                              :metadata {}}})]]
-    {:command (rand-nth (flatten command-options))
-     :state {:test true}}))
+  (let [command-options [(repeat 10 {:action :move
+                                     :metadata {}})
+                         (repeat 2 {:action :turn
+                                    :metadata {:direction (rand-nth turn-directions)}})
+                         (repeat 4 {:action :shoot
+                                      :metadata {}})
+                         (repeat 3 {:action :smoke
+                                    :metadata {}})]]
+
+    #_{:command (rand-nth (flatten command-options))
+       :state {:test true}}
+
+    {:command {:action :smoke
+               :metadata {}}
+     :state {}}))
