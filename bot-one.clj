dfa27d17211deea4009334c893a07e0c2b9a4c67
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-02-09 16:15:30 -0500

    update weights

--- bot-one.clj
+++ bot-one.clj
@@ -2,13 +2,13 @@
   (def turn-directions [:right :left :about-face])
   (def smoke-directions [:forward :backward :left :right :drop])
 
-  (let [command-options [(repeat 10 {:action :move
+  (let [command-options [(repeat 8 {:action :move
                                      :metadata {}})
                          (repeat 2 {:action :turn
                                     :metadata {:direction (rand-nth turn-directions)}})
                          (repeat 4 {:action :shoot
                                       :metadata {}})
-                         (repeat 3 {:action :smoke
+                         (repeat 1 {:action :smoke
                                     :metadata {:direction (rand-nth smoke-directions)}})]]
 
     {:command (rand-nth (flatten command-options))
