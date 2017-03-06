3605f98ece5ab9266bab82c721c1b9a32914ce6b
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-03-06 09:57:54 -0500

    Update bot.clj

--- bot.clj
+++ bot.clj
@@ -1,15 +1,14 @@
-(fn [state time-left]
-  (def turn-directions [:right :left :about-face])
-  (def smoke-directions [:forward :backward :left :right :drop])
-
-  (let [command-options [(repeat 8 {:action :move
-                                     :metadata {}})
-                         (repeat 2 {:action :turn
-                                    :metadata {:direction (rand-nth turn-directions)}})
-                         (repeat 4 {:action :shoot
-                                      :metadata {}})
-                         (repeat 1 {:action :smoke
-                                    :metadata {:direction (rand-nth smoke-directions)}})]]
-
-    {:command (rand-nth (flatten command-options))
-     :state {}}))
+(defn run
+  [{:keys  [arena state bot_id energy spawn-bot?] :as step-details}]
+  (let [command-options [[{:cmd "SMOKESCREEN"}]
+                         [{:cmd  "SHOOT"
+                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]
+                         [{:cmd  "MOVE"
+                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]
+                         [{:cmd  "MOVE"
+                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]
+                         [{:cmd  "MOVE"
+                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]
+                         [{:cmd  "MOVE"
+                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]]]
+    {:commands (rand-nth command-options)}))
