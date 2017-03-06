3a494373206ed7643f793882f2bcce7380c4e845
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-03-06 09:56:57 -0500

    Create bot.clj

---
+++ bot.clj
@@ -0,0 +1,15 @@
+(fn [state time-left]
+  (def turn-directions [:right :left :about-face])
+  (def smoke-directions [:forward :backward :left :right :drop])
+
+  (let [command-options [(repeat 8 {:action :move
+                                     :metadata {}})
+                         (repeat 2 {:action :turn
+                                    :metadata {:direction (rand-nth turn-directions)}})
+                         (repeat 4 {:action :shoot
+                                      :metadata {}})
+                         (repeat 1 {:action :smoke
+                                    :metadata {:direction (rand-nth smoke-directions)}})]]
+
+    {:command (rand-nth (flatten command-options))
+     :state {}}))
