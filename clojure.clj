dbd6a478044a533e67ca59539233018dc2705f43
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-03-08 19:09:07 -0500

    add clojure bot

---
+++ clojure.clj
@@ -0,0 +1,351 @@
+(def sample-arena
+  [[{:contents {:type "fog"}}
+  {:contents {:type "fog"}}
+  {:contents {:type "fog"}}
+  {:contents {:type "fog"}}
+  {:contents {:type "fog"}}
+  {:contents {:type "fog"}}
+  {:contents {:type "fog"}}]
+ [{:contents
+   {:type "wood-barrier",
+    :hp 20,
+    :uuid "174cb9d2-5634-48fb-9e9d-1a256cc3ad04",
+    :deterioration-level "low"},
+   :meta []}
+  {:contents
+   {:type "wood-barrier",
+    :hp 20,
+    :uuid "ba4cacd8-438f-41ab-8978-948281157691",
+    :deterioration-level "low"},
+   :meta []}
+  {:contents
+   {:type "wood-barrier",
+    :hp 20,
+    :uuid "11fefbb1-cd48-4bf9-9b11-0df1ed51b1c8",
+    :deterioration-level "low"},
+   :meta []}
+  {:contents
+   {:type "wood-barrier",
+    :hp 20,
+    :uuid "2dd14184-1bdf-421d-9bdf-3914df66dbe6",
+    :deterioration-level "low"},
+   :meta []}
+  {:contents
+   {:type "wood-barrier",
+    :hp 20,
+    :uuid "70a4ee76-061c-4c47-8a0a-58b2cd4ef25c",
+    :deterioration-level "low"},
+   :meta []}
+  {:contents
+   {:type "wood-barrier",
+    :hp 20,
+    :uuid "111e951e-9950-43eb-9981-5a1a9b44c1d4",
+    :deterioration-level "low"},
+   :meta []}
+  {:contents
+   {:type "wood-barrier",
+    :hp 20,
+    :uuid "4fb7a428-e3d8-4b81-9e49-9f084441b953",
+    :deterioration-level "low"},
+   :meta []}]
+ [{:contents
+   {:type "food", :uuid "d13b18eb-6c27-44a1-b88b-7e9b4d0b022a"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+   :meta []}
+  {:contents
+   {:type "food", :uuid "a3a8a883-6f0a-4b4e-980d-8fbf5355029e"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+   :meta []}]
+ [{:contents
+   {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+   :meta []}
+  {:contents
+   {:type "wombat",
+    :uuid "3f7f33fb-d16d-4b0b-a948-657bf864e0fa",
+    :color "gray",
+    :hp 100,
+    :orientation "e",
+    :deterioration-level "low"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
+   :meta []}
+  {:contents
+   {:type "poison", :uuid "e5e2937a-badd-440c-bee3-c9ef65818d97"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+   :meta []}]
+ [{:contents
+   {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "19817042-e591-488a-ae91-8b5c993d87ec"},
+   :meta []}
+  {:contents
+   {:type "poison", :uuid "bffe627e-f7c3-4ea2-9762-fa5ac14a82ad"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+   :meta []}]
+ [{:contents
+   {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "19817042-e591-488a-ae91-8b5c993d87ec"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+   :meta []}]
+ [{:contents
+   {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "19817042-e591-488a-ae91-8b5c993d87ec"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
+   :meta []}
+  {:contents
+   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+   :meta []}]])
+
+(defn get-arena-dimensions
+  "returns the dimensions of a given arena (NOTE: Not 0 based)"
+  {:added "1.0"
+   :defined-in "wombats.arena.utils"}
+  [arena]
+  (let [x ((comp count first) arena)
+        y (count arena)]
+    [x y]))
+
+(defn- normalize-slope
+  {:added "1.0"
+   :defined-in "wombats.game.utils"}
+  [coords start]
+  (if (= (vec (first coords)) start)
+    (vec coords)
+    (vec (reverse coords))))
+
+(defn draw-line
+  "Draw a line from x1,y1 to x2,y2 using Bresenham's line algorithm.
+
+    Modified from http://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#Clojure"
+  {:added "1.0"
+   :defined-in "wombats.game.utils"}
+  [from to]
+  (let [[x1 y1] from
+        [x2 y2] to
+        dist-x (Math/abs (- x1 x2))
+        dist-y (Math/abs (- y1 y2))
+        steep (> dist-y dist-x)]
+    (let [[x1 y1 x2 y2] (if steep
+                          [y1 x1 y2 x2]
+                          [x1 y1 x2 y2])]
+      (let [[x1 y1 x2 y2] (if (> x1 x2)
+                            [x2 y2 x1 y1]
+                            [x1 y1 x2 y2])]
+        (let  [delta-x (- x2 x1)
+               delta-y (Math/abs (- y1 y2))
+               y-step (if (< y1 y2) 1 -1)]
+          (loop [x x1
+                 y y1
+                 error (Math/floor (/ delta-x 2))
+                 plots [[x y]]]
+            (if (< x x2)
+              (let [[x y err] (if (< error delta-y)
+                                [(inc x) (+ y y-step) (+ error (- delta-x delta-y))]
+                                [(inc x) y (- error delta-y)])]
+                (recur x y err (conj plots [x y])))
+              (if steep
+                (normalize-slope (map (fn [[y x]] [x y]) plots) from)
+                (normalize-slope plots from)))))))))
+
+
+(defn- get-in-arena
+  [[x y] arena]
+  (get-in arena [y x]))
+
+(def ^:private
+  orientations
+  [:n :s :e :w])
+
+(defn- modify-orientation
+  ""
+  {:added "1.0"
+   :defined "wombats.game.utils"}
+  [current-orientation modifier]
+  (let [current-idx (.indexOf orientations current-orientation)]
+    (if (not= current-idx -1)
+      (condp = modifier
+          :right (get orientations (mod (inc current-idx) 4))
+          :left (get orientations (mod (dec current-idx) 4))
+          :about-face (get orientations (mod (+ 2 current-idx) 4))
+          current-orientation)
+      current-orientation)))
+
+(defn- get-move-frontier
+  "Returns the coords from the move command"
+  ([coords orientation dimensions]
+   (get-move-frontier coords orientation dimensions false))
+  ([[x y] orientation [max-x max-y] wrap?]
+   (let [new-coords (case orientation
+                      :n [x (dec y)]
+                      :e [(inc x) y]
+                      :s [x (inc y)]
+                      :w [(dec x) y])
+         [new-x new-y] new-coords]
+     (if wrap?
+       (case orientation
+         (:n :s) [new-x (mod new-y max-y)]
+         (:e :w) [(mod new-x max-x) new-y])
+       (case orientation
+         :n (if (< new-y 0) nil new-coords)
+         :w (if (< new-x 0) nil new-coords)
+         :e (if (> new-x max-x) nil new-coords)
+         :s (if (> new-y max-y) nil new-coords))))))
+
+(defn- calculate-frontier
+  "Caclulates the new frontier set based off of the provided frontier."
+  {:added "1.0"}
+  ([frontier arena-dimensions]
+   (calculate-frontier frontier arena-dimensions false))
+  ([{:keys [orientation coords weight move-cmd]} arena-dimensions wrap?]
+   (let [frontier-orientations
+         (map (fn [next-direction]
+                (cond-> {:orientation (modify-orientation orientation next-direction)
+                         :coords coords
+                         :weight (inc weight)}
+                  (nil? move-cmd) (merge {:move-cmd
+                                          {:action :turn
+                                           :metadata {:direction next-direction}}})))
+              (if (= weight 0)
+                [:right :left :about-face]
+                [:right :left]))
+
+         frontier-move
+         (cond-> {:orientation orientation
+                  :coords (get-move-frontier coords orientation arena-dimensions wrap?)
+                  :weight (inc weight)}
+           (nil? move-cmd) (merge {:move-cmd {:action :move}}))]
+     (conj frontier-orientations frontier-move))))
+
+(defn- can-safely-occupy-space?
+  "Predicate used to determine what cells can pass as frontiers"
+  {:added "1.0"}
+  [cell]
+  (not (contains? #{"wood-barrier"
+                    "steel-barrier"
+                    "fog"} (get-in cell [:contents :type]))))
+
+(defn- filter-frontier
+  "Filters all the possible frontiers, returning only explore-able frontiers"
+  {:added "1.0"}
+  [frontier arena explored]
+  (filter (fn [{coords :coords}]
+            (let [cell (get-in-arena coords arena)
+                  uuid (get-in cell [:contents :uuid])]
+              (and (nil? (get explored uuid))
+                   (can-safely-occupy-space? cell)))) frontier))
+
+(defn sort-arena-by-distance-then-type
+  "sorts an arena by distance then type"
+  {:added "1.0"}
+  [arena origin]
+  (let [arena-dimensions (get-arena-dimensions arena)
+        {{orientation-str :orientation
+          uuid :uuid} :contents} (get-in-arena origin arena)
+        orientation (keyword orientation-str)]
+    (loop [frontier [{:coords origin
+                      :orientation orientation
+                      :uuid uuid
+                      :weight 0
+                      :move-cmd nil}]
+           explored {}
+           sorted-arena []]
+
+      (if (empty? frontier)
+        sorted-arena
+        ;; Get first item off of frontier
+        (let [frontier-node (first frontier)
+              cell (get-in-arena (:coords frontier-node) arena)
+              next-frontier (calculate-frontier frontier-node arena-dimensions)
+              filtered-frontier (filter-frontier next-frontier arena explored)]
+
+          (recur (vec (concat (rest frontier) filtered-frontier))
+                 (merge explored {(get-in cell [:contents :uuid]) true})
+                 sorted-arena))))))
+
+;; Evaluate each expresion with C-x C-e and then evaluate the
+;; following to test the algorithm
+(prn (sort-arena-by-distance-then-type sample-arena [3 3]))
+
+(fn [state time-left]
+
+  (defn- sort-arena
+    [{:keys [arena local-coords] :as enriched-state}]
+    (assoc enriched-state :sorted-arena (sort-arena-by-distance-than-type arena
+                                                                          local-coords)))
+
+  (defn enrich-state
+    "Adds additional information to the given state used to improve
+     the decision-making process"
+    {:added "1.0"}
+    [{:keys [arena] :as state}]
+    (-> state
+        (assoc :dimensions (get-arena-dimensions arena))
+        (sort-arena)
+        (dissoc :saved-state)))
+
+  {:command {:action :turn
+             :metadata {:direction :right}}
+   :state (enrich-state state)})
