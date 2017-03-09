c923770e5e0d20f5ebdc821cfacb9104d5f85046
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-03-08 21:15:30 -0500

    update clojure bot

--- clojure.clj
+++ clojure.clj
@@ -1,163 +1,163 @@
 (def sample-arena
   [[{:contents {:type "fog"}}
-  {:contents {:type "fog"}}
-  {:contents {:type "fog"}}
-  {:contents {:type "fog"}}
-  {:contents {:type "fog"}}
-  {:contents {:type "fog"}}
-  {:contents {:type "fog"}}]
- [{:contents
-   {:type "wood-barrier",
-    :hp 20,
-    :uuid "174cb9d2-5634-48fb-9e9d-1a256cc3ad04",
-    :deterioration-level "low"},
-   :meta []}
-  {:contents
-   {:type "wood-barrier",
-    :hp 20,
-    :uuid "ba4cacd8-438f-41ab-8978-948281157691",
-    :deterioration-level "low"},
-   :meta []}
-  {:contents
-   {:type "wood-barrier",
-    :hp 20,
-    :uuid "11fefbb1-cd48-4bf9-9b11-0df1ed51b1c8",
-    :deterioration-level "low"},
-   :meta []}
-  {:contents
-   {:type "wood-barrier",
-    :hp 20,
-    :uuid "2dd14184-1bdf-421d-9bdf-3914df66dbe6",
-    :deterioration-level "low"},
-   :meta []}
-  {:contents
-   {:type "wood-barrier",
-    :hp 20,
-    :uuid "70a4ee76-061c-4c47-8a0a-58b2cd4ef25c",
-    :deterioration-level "low"},
-   :meta []}
-  {:contents
-   {:type "wood-barrier",
-    :hp 20,
-    :uuid "111e951e-9950-43eb-9981-5a1a9b44c1d4",
-    :deterioration-level "low"},
-   :meta []}
-  {:contents
-   {:type "wood-barrier",
-    :hp 20,
-    :uuid "4fb7a428-e3d8-4b81-9e49-9f084441b953",
-    :deterioration-level "low"},
-   :meta []}]
- [{:contents
-   {:type "food", :uuid "d13b18eb-6c27-44a1-b88b-7e9b4d0b022a"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
-   :meta []}
-  {:contents
-   {:type "food", :uuid "a3a8a883-6f0a-4b4e-980d-8fbf5355029e"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
-   :meta []}]
- [{:contents
-   {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
-   :meta []}
-  {:contents
-   {:type "wombat",
-    :uuid "3f7f33fb-d16d-4b0b-a948-657bf864e0fa",
-    :color "gray",
-    :hp 100,
-    :orientation "e",
-    :deterioration-level "low"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
-   :meta []}
-  {:contents
-   {:type "poison", :uuid "e5e2937a-badd-440c-bee3-c9ef65818d97"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
-   :meta []}]
- [{:contents
-   {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "19817042-e591-488a-ae91-8b5c993d87ec"},
-   :meta []}
-  {:contents
-   {:type "poison", :uuid "bffe627e-f7c3-4ea2-9762-fa5ac14a82ad"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
-   :meta []}]
- [{:contents
-   {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "19817042-e591-488a-ae91-8b5c993d87ec"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
-   :meta []}]
- [{:contents
-   {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "19817042-e591-488a-ae91-8b5c993d87ec"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
-   :meta []}
-  {:contents
-   {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
-   :meta []}]])
+    {:contents {:type "fog"}}
+    {:contents {:type "fog"}}
+    {:contents {:type "fog"}}
+    {:contents {:type "fog"}}
+    {:contents {:type "fog"}}
+    {:contents {:type "fog"}}]
+   [{:contents
+     {:type "wood-barrier",
+      :hp 20,
+      :uuid "174cb9d2-5634-48fb-9e9d-1a256cc3ad04",
+      :deterioration-level "low"},
+     :meta []}
+    {:contents
+     {:type "wood-barrier",
+      :hp 20,
+      :uuid "ba4cacd8-438f-41ab-8978-948281157691",
+      :deterioration-level "low"},
+     :meta []}
+    {:contents
+     {:type "wood-barrier",
+      :hp 20,
+      :uuid "11fefbb1-cd48-4bf9-9b11-0df1ed51b1c8",
+      :deterioration-level "low"},
+     :meta []}
+    {:contents
+     {:type "wood-barrier",
+      :hp 20,
+      :uuid "2dd14184-1bdf-421d-9bdf-3914df66dbe6",
+      :deterioration-level "low"},
+     :meta []}
+    {:contents
+     {:type "wood-barrier",
+      :hp 20,
+      :uuid "70a4ee76-061c-4c47-8a0a-58b2cd4ef25c",
+      :deterioration-level "low"},
+     :meta []}
+    {:contents
+     {:type "wood-barrier",
+      :hp 20,
+      :uuid "111e951e-9950-43eb-9981-5a1a9b44c1d4",
+      :deterioration-level "low"},
+     :meta []}
+    {:contents
+     {:type "wood-barrier",
+      :hp 20,
+      :uuid "4fb7a428-e3d8-4b81-9e49-9f084441b953",
+      :deterioration-level "low"},
+     :meta []}]
+   [{:contents
+     {:type "food", :uuid "d13b18eb-6c27-44a1-b88b-7e9b4d0b022a"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+     :meta []}
+    {:contents
+     {:type "food", :uuid "a3a8a883-6f0a-4b4e-980d-8fbf5355029e"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+     :meta []}]
+   [{:contents
+     {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+     :meta []}
+    {:contents
+     {:type "wombat",
+      :uuid "3f7f33fb-d16d-4b0b-a948-657bf864e0fa",
+      :color "gray",
+      :hp 100,
+      :orientation "e",
+      :deterioration-level "low"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
+     :meta []}
+    {:contents
+     {:type "poison", :uuid "e5e2937a-badd-440c-bee3-c9ef65818d97"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+     :meta []}]
+   [{:contents
+     {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "19817042-e591-488a-ae91-8b5c993d87ec"},
+     :meta []}
+    {:contents
+     {:type "poison", :uuid "bffe627e-f7c3-4ea2-9762-fa5ac14a82ad"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+     :meta []}]
+   [{:contents
+     {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "19817042-e591-488a-ae91-8b5c993d87ec"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+     :meta []}]
+   [{:contents
+     {:type "open", :uuid "c8d35c13-e34a-4aa5-b8b1-62f90a4cf704"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "32556f3e-4c66-4645-9b20-71932faaf29f"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "d1b70537-8614-4cc2-881d-cded36ecae17"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "19817042-e591-488a-ae91-8b5c993d87ec"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "ad517c6c-edc4-4a28-903c-d81818b4572b"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "124b471e-722c-471c-9771-8a35cf68be28"},
+     :meta []}
+    {:contents
+     {:type "open", :uuid "b8ec04d4-7855-44fa-b600-eeba75b9f3be"},
+     :meta []}]])
 
 (def ^:private
   orientations
@@ -190,10 +190,10 @@
   (let [current-idx (.indexOf orientations current-orientation)]
     (if (not= current-idx -1)
       (condp = modifier
-          :right (get orientations (mod (inc current-idx) 4))
-          :left (get orientations (mod (dec current-idx) 4))
-          :about-face (get orientations (mod (+ 2 current-idx) 4))
-          current-orientation)
+        :right (get orientations (mod (inc current-idx) 4))
+        :left (get orientations (mod (dec current-idx) 4))
+        :about-face (get orientations (mod (+ 2 current-idx) 4))
+        current-orientation)
       current-orientation)))
 
 (defn- get-move-frontier
@@ -267,6 +267,24 @@
                 (and (nil? (get explored uuid))
                      (can-safely-occupy-space? cell))))) frontier))
 
+(defn- add-to-sorted-arena
+  "Adds a frontier node to the sorted arena"
+  {:added "1.0"}
+  [sorted-arena
+   {{type :type
+     uuid :uuid} :contents}
+   {weight :weight
+    move-cmd :move-cmd}]
+  (let [formatted-frontier {:move-cmd move-cmd
+                            :weight weight
+                            :uuid uuid}]
+    (update-in sorted-arena
+               [weight (keyword type)]
+               (fn [coll]
+                 (if (not (nil? coll))
+                   (conj coll formatted-frontier)
+                   [formatted-frontier])))))
+
 (defn sort-arena-by-distance-then-type
   "sorts an arena by distance then type"
   {:added "1.0"}
@@ -293,13 +311,13 @@
 
           (recur (vec (concat (rest frontier) filtered-frontier))
                  (merge explored {(get-in cell [:contents :uuid]) true})
-                 ;; TODO: Push cell onto sorted-arena
-                 sorted-arena))))))
+                 (add-to-sorted-arena sorted-arena cell frontier-node)))))))
 
 ;; Evaluate each expresion with C-x C-e and then evaluate the
 ;; following to test the algorithm
 (clojure.pprint/pprint
  (sort-arena-by-distance-then-type sample-arena [3 3]))
+
 
 (fn [state time-left]
 
